7c062fafcc4cbda27ee4965ae858407f9d56b8fa
Bill Hunt <bill@krues8dr.com>
2019-06-03 22:06:36 -0400

    Add examples to homepage.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,11 +9,22 @@
     One of the biggest hurdles for government agencies to embrace <B>Open Data</B> is the ability to produce documents in a government-usable format, but well-formatted data and machine-readable text are not government-friendly formats.
 </P>
 <P>
-    To solve this, we created <B>Govify</B>, a tool which converts machine-friendly text into Government-ready PDFs.
+    To solve this, we created <B>Govify</B>, a tool which converts machine-friendly text into Government-ready PDFs. These legible but closed-format documents can be easily used for any government purpose - or even printed, signed, and re-scanned to add extra bureaucracy.
 </P>
 <P>
-    These legible but closed-format documents can be easily used for any government purpose - or even printed, signed, and re-scanned to add extra bureaucracy.
+    <B>NEW: Govify now automatically redacts technical documents to remove some proprietary information!</B>
 </P>
+<CENTER>
+<TABLE>
+    <TR>
+        <TD><CENTER><IMG SRC="/img/before-govify.gif" ALT="Screenshot: Before Govify"/><BR/>
+        <B>Before</B></CENTER></TD>
+        <TD ROWSPAN=2>&nbsp;&nbsp;&nbsp;&rarr;&nbsp;&nbsp;&nbsp;</TD>
+        <TD><CENTER><IMG SRC="/img/after-govify.gif" ALT="Screenshot: After Govify"/><BR/>
+        <B>After</B></CENTER></TD>
+    </TR>
+</TABLE>
+</CENTER>
 <P>
     <A HREF="/docs">Click here to view documents people have uploaded.</A>
 </P>
