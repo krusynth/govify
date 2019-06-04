ca31dc2c56184bc5a245c130a0fddda4919a6305
Bill Hunt <bill@krues8dr.com>
2019-06-04 01:50:45 +0000

    Remove high-ascii redaction which broke some docs. Swap font name to Ubuntu standard.

--- bin/govify.sh
+++ bin/govify.sh
@@ -10,11 +10,11 @@ pandoc -t plain+ignore_line_breaks $1 -o $1.txt
 
 awk -i inplace ' /^$/ { print "\n"; } /./ { printf("%s ", $0); } END { print ""; } ' $1.txt
 
-sed -Ei "s/(agile|cloud|blockchain|technology|office|data|taxpayer)/████████████/gI" $1.txt
+sed -Ei "s/(agile|cloud|blockchain|technology|office|data|taxpayer)/XXXXXXXX/gI" $1.txt
 
 cat $1.txt | fold | \
 convert \
--font CourierNew \
+-font Courier-New \
 text:- \
 -rotate `convert null: -format '%[fx:rand()*15-7]' info:` \
 -attenuate 0.25 \
